77d5664bb39ea3491456889867a1d2f2e4b03ad3
Jeonghyun Ryu <jhforstudy@gmail.com>
2022-10-04 16:27:32 +0900

    유정현_과제_1

--- 유정현/유정현_과제_1.py
+++ 유정현/유정현_과제_1.py
@@ -1,2 +1,19 @@
-# 이런식으로
-# 올려주세요
+class Calculator:
+    def __init__(self):
+        self.value = 0
+
+    def add(self, val):
+        self.value += val
+
+class MaxLimitCalculator(Calculator):
+    def add(self, val):
+        self.value += val
+        if self.value >= 100:
+            self.value = 100
+
+if __name__ == "__main__":
+    cal = MaxLimitCalculator()
+    cal.add(50)
+    print(cal.value)
+    cal.add(60)
+    print(cal.value)
